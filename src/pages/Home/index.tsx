361d564e815238e1ba58b5a4de374be03b57cc07
n-ndrd <ncamilada15@gmail.com>
2024-01-14 18:36:27 -0300

    react-router-dom e home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,51 @@
+import { Play } from "phosphor-react";
+import { 
+    StartCountdownButton,
+    CountdownContainer, 
+    FormContainer, 
+    HomeContainer, 
+    Separator, 
+    MinutesAmountInput,
+    TaskInput
+} from "./styles";
+
+export function Home() {
+    return (
+        <HomeContainer>
+            <form action="submit">
+                <FormContainer>
+                    <label htmlFor="task">Vou trabalhar em</label>
+                    <TaskInput 
+                        id="task" 
+                        placeholder="Dê um nome ao seu projeto"
+                    />
+
+                    <label htmlFor="task">durante</label>
+                    <MinutesAmountInput 
+                        type="number" 
+                        id="minutesAmount" 
+                        placeholder="00"
+                    />
+
+                    <span>minutos.</span>
+                </FormContainer>
+
+                <CountdownContainer>
+                    <span>0</span>
+                    <span>0</span>
+                    <Separator>:</Separator>
+                    <span>0</span>
+                    <span>0</span>
+                </CountdownContainer>
+
+                <StartCountdownButton 
+                    disabled
+                    type="submit"
+                >
+                    <Play size={24}/>
+                    Começar
+                </StartCountdownButton>
+            </form>
+        </HomeContainer>
+    )
+}
